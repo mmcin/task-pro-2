7e2d97d1d4ed9a78eadca549798b94b3f46805d6
GavPri <gavinprior5@gmail.com>
2023-10-11 14:48:33 +0000

    update allowed hosts

--- task_pro/settings.py
+++ task_pro/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-gavpri-taskpro-iwtvs1krh0z.ws-eu105.gitpod.io', 'task-pro.herokuapp.com']
+ALLOWED_HOSTS = ['8000-gavpri-taskpro-iwtvs1krh0z.ws-eu105.gitpod.io', 'https://task-pro-1fa2a517fe32.herokuapp.com/']
 
 
 # Application definition
