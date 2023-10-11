91cd38a9cf5b73b63daeb1650ca87b9a5b233539
GavPri <gavinprior5@gmail.com>
2023-10-11 17:55:57 +0000

    animation of icon on theme change

--- static/js/app.js
+++ static/js/app.js
@@ -4,19 +4,35 @@ const tooltip = new bootstrap.Tooltip(themeToolTip);
 
 // ----- Script For Changing The Theme -----
 
-// Local storgage to save visitor preferences & Button to chnage theme
+// Local storgage to save visitor preferences, button to change theme & Icon
 let darkMode = localStorage.getItem("darkMode");
 const themeChangeButton = document.querySelector("#theme-tooltip");
+const themeChangeIcon = document.querySelector(".theme-toggle");
 
 // Function to enable darkmode.
 const enableDarkMode = () => {
+  //  Add darkmode display to the body
   document.body.classList.add("darkmode");
+  //   Animation Spin & change.
+  themeChangeIcon.classList.add("rotate-icon");
+  themeChangeIcon.classList.add("fa-moon");
+  themeChangeIcon.classList.remove("fa-sun");
+  //   Local storage update
   localStorage.setItem("darkMode", "enabled");
 };
 
 // Function to disable darkmode
 const disableDarkMode = () => {
+  //  Add darkmode display to the body
   document.body.classList.remove("darkmode");
+  //   Animation Spin & change.
+  themeChangeIcon.classList.add("rotate-icon");
+  themeChangeIcon.classList.remove("fa-moon");
+  themeChangeIcon.classList.add("fa-sun");
+  setTimeout(() => {
+    themeChangeIcon.classList.remove("rotate-icon");
+  }, 0);
+  //   Local storage update
   localStorage.setItem("darkMode", null);
 };
 
