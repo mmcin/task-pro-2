7150df2e2dee42d09cfb4580240c241352bf6dc6
GavPri <gavinprior5@gmail.com>
2023-10-14 15:47:35 +0000

    add task functionality

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import path
+from .views import AddTaskView
 
 
 urlpatterns = [
-    path('', views.TaskListView.as_view(), name='home')
+    path('', views.TaskListView.as_view(), name='home'),
+    path('add/', AddTaskView.as_view(), name='add_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Import generic and Task Model
 from django.views import generic
 from .models import Task
+from django.views import View
 
 
 class TaskListView(generic.ListView):
@@ -13,3 +14,27 @@ class TaskListView(generic.ListView):
         return Task.objects.filter(user=self.request.user).values(
             'title', 'description', 'due_date', 'urgent', 'completed'
         )
+
+
+class AddTaskView(View):
+    template_name = 'add_task.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        # Process the form submission here
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        urgent = request.POST.get('urgent', False) == 'on'
+
+        # Create a new Task object
+        task = Task(
+            user=request.user,
+            title=title,
+            description=description,
+            urgent=urgent
+            )
+        task.save()
+
+        return redirect('home')
