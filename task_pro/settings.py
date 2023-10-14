b777c00b10dcd65a7376245d710df21d0831c388
GavPri <gavinprior5@gmail.com>
2023-10-14 11:09:01 +0000

    boiler plate html for hero section

--- task_pro/settings.py
+++ task_pro/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-gavpri-taskpro2-uvuis3cem4i.ws-eu105.gitpod.io',
+    '8000-gavpri-taskpro2-1smv1tfmw30.ws-eu105.gitpod.io',
     'task-pro-1fa2a517fe32.herokuapp.com',
     'task-pro.herokuapp.com']
 
