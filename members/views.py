6a01850a5f5fd4ae27c976c265d96c737e96620b
GavPri <gavinprior5@gmail.com>
2023-10-15 08:40:46 +0000

    start view and link for login

--- members/views.py
+++ members/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# User Authentification
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages 
 
-# Create your views here.
+
+def login_user(request):
+    return render(request, 'authenticate/login.html', {}),
