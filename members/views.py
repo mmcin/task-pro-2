54a08b52027677fc83b3b5bc3f3bada1840cd439
GavPri <gavinprior5@gmail.com>
2023-10-15 09:01:11 +0000

    update login view

--- members/views.py
+++ members/views.py
@@ -4,5 +4,28 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages 
 
 
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+
 def login_user(request):
-    return render(request, 'authenticate/login.html', {})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Authenticate the user
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            # Log in the user
+            login(request, user)
+            return redirect('view_tasks')
+        else:
+            messages.error(
+                request, "There was an error logging in. Please try again."
+                )
+            return redirect('home')
+
+    else:
+        return render(request, 'authenticate/login.html', {})
