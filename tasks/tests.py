5ff95bcf12aeb37cf6b3779563b232bcd3a88bc7
GavPri <gavinprior5@gmail.com>
2023-10-16 19:02:07 +0000

    check that logged in users can view edit task template

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,8 +1,9 @@
 import unittest
-from django.test import Client
+from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
 from .models import Task
+
 
 class TaskListViewTest(unittest.TestCase):
 
@@ -10,7 +11,8 @@ class TaskListViewTest(unittest.TestCase):
         # Create a test client
         self.client = Client()
         # Creating a test user
-        self.user = User.objects.create_user(username='testuser3', password='testpassword')
+        self.user = User.objects.create_user(
+            username='testuser3', password='testpassword')
         # Log the user in
         self.client.login(username='testuser3', password='testpassword')
         # Create a task
@@ -50,6 +52,19 @@ class TaskListViewTest(unittest.TestCase):
         # Check if the task was actually created in the database
         self.assertTrue(
             Task.objects.filter(title='Test Task', user=self.user).exists())
+
+    # Check That Logged In Users Can Access The Edit Task Views
+    def test_view_access_to_edit_when_logged_in(self):
+        # Create a task before attempting to access the 'edit' view
+        task = Task.objects.create(user=self.user, title='Test Task')
+
+        url = reverse('edit', args=[str(task.id)])
+        response = self.client.get(url)
+
+        # Clean up the created task after the test
+        task.delete()
+
+        self.assertEqual(response.status_code, 200)
 
     def tearDown(self):
         # Clean up any test data as needed
