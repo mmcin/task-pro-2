2105fab837793c3969a058cb41e62caf68be487c
GavPri <gavinprior5@gmail.com>
2023-10-16 19:50:35 +0000

    create test to edit task

--- tasks/tests.py
+++ tasks/tests.py
@@ -12,10 +12,18 @@ class TaskListViewTest(unittest.TestCase):
         self.client = Client()
         # Creating a test user
         self.user = User.objects.create_user(
-            username='testuser3', password='testpassword')
+            username='testuser101', password='testpassword')
         # Log the user in
-        self.client.login(username='testuser3', password='testpassword')
+        self.client.login(username='testuser101', password='testpassword')
         # Create a task
+        self.task = Task.objects.create(
+            user=self.user,
+            title='Test Task',
+            description='Original description',
+            urgent=False,
+            completed=False,
+        )
+        # Create a task id
         self.task_id = 1
 
     # Check That Anyone Can Access The Home Page
@@ -38,33 +46,54 @@ class TaskListViewTest(unittest.TestCase):
         response = self.client.post(url, {
             'title': 'Test Task',
             'description': 'This is a test task.',
-            'urgent': 'on',  # Simulating a checkbox being checked
-            'completed': 'on'  # Simulating a checkbox being checked
+            'urgent': 'on',
+            'completed': 'on'
         })
 
-        # Check if the task was created successfully
         self.assertEqual(response.status_code, 302)
         redirect_url = reverse('view_tasks')
 
-        # Check if the response redirects to the expected URL
         self.assertEqual(response.url, redirect_url)
 
-        # Check if the task was actually created in the database
         self.assertTrue(
             Task.objects.filter(title='Test Task', user=self.user).exists())
 
     # Check That Logged In Users Can Access The Edit Task Views
     def test_view_access_to_edit_when_logged_in(self):
         # Create a task before attempting to access the 'edit' view
-        task = Task.objects.create(user=self.user, title='Test Task')
+        self.task = Task.objects.create(user=self.user, title='Test Task')
 
-        url = reverse('edit', args=[str(task.id)])
+        url = reverse('edit', args=[int(self.task.id)])
         response = self.client.get(url)
 
         # Clean up the created task after the test
-        task.delete()
+        self.task.delete()
 
         self.assertEqual(response.status_code, 200)
+
+    # Test if task can be eddited 
+    def test_edit_task(self):
+        task = self.task
+        url = reverse('edit', args=[str(self.task.id)])
+        updated_title = 'Updated Test Task'
+        updated_description = 'Updated description'
+
+        # Simulate a POST request to update the task
+        response = self.client.post(url, {
+            'title': updated_title,
+            'description': updated_description,
+            'urgent': 'on',
+            'completed': 'on',
+        })
+
+        # Check if the response redirects to the expected URL
+        self.assertEqual(response.status_code, 302)
+
+        redirect_url = reverse('view_tasks')
+        self.assertEqual(response.url, redirect_url)
+
+        # Refresh the task from the database
+        self.task.refresh_from_db()
 
     def tearDown(self):
         # Clean up any test data as needed
